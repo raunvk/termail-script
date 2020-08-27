a3116a9be7cc3c5b8f9f83e73da2eee40b210ab8
Raunak Das <it1811@cemk.ac.in>
2020-08-27 12:53:00 +0530

    v1.0

--- mail_bomb.py
+++ mail_bomb.py
@@ -3,20 +3,18 @@ import webbrowser
 import sys
 import time
 import getpass
+from colored import fg, bg, attr
 
-class bcolors:
-	OKGREEN = '\033[92m'
-	WARNING = '\033[93m'
-	FAIL = '\033[91m'
-	ENDC = '\033[0m'
+color = fg('green')
+reset = attr('reset')
 
 try:
 	file1 = open('mail_bomb.txt', 'r')
 	print(' ')
-	print (bcolors.OKGREEN + file1.read() + bcolors.ENDC)
+	print (color + file1.read() + reset)
 	file1.close()
 except IOError:
-	print('\nBanner File not found!!!')
+	print('\nBanner File not found!')
 
 
 userid = input("\nEnter your G-Mail id :\t")
@@ -53,4 +51,3 @@ for i in range (0, count):
 		print ("\nFailed to Send!!!\n")
 
 server.close()
-
